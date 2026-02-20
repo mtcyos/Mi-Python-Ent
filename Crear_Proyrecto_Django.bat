a31b896953e7c604eeb38e53fcbf384686064497
Miguel Tortosa <mtcyos@yahoo.es>
2026-02-20 11:05:09 -0600

    Django

---
+++ Crear_Proyrecto_Django.bat
@@ -0,0 +1,9 @@
+# Entra a la carpeta de tus fuentes
+cd /d D:\fuentes_prg\Python\Pruebas\
+
+# Usa tu Python específico para crear el proyecto
+# (Ajusta la ruta de python.exe a donde lo tengas tú)
+"d:\fuentes_prg\Python\_Python\python.exe" -m django startproject Django_Pruebas
+
+"d:\fuentes_prg\Python\_Python\python.exe" D:\fuentes_prg\Python\Pruebas\Django_Pruebas\manage.py startapp web
+pause
